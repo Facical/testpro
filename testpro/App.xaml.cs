960f47bf6c55f68234b7c281eeba00f0c246ae02
Facical <joh0816@gmail.com>
2025-06-10 18:18:15 +0900

    초기 커밋.

---
+++ testpro/App.xaml.cs
@@ -0,0 +1,68 @@
+﻿// App.xaml.cs에 추가 - 전역 UI 성능 모니터링
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Threading;
+
+public partial class App : Application
+{
+    private DispatcherTimer _performanceTimer;
+    private int _frameCount = 0;
+    private DateTime _lastFrameTime = DateTime.Now;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // 프레임 렌더링 모니터링
+        CompositionTarget.Rendering += OnRendering;
+
+        // 1초마다 FPS 계산
+        _performanceTimer = new DispatcherTimer();
+        _performanceTimer.Interval = TimeSpan.FromSeconds(1);
+        _performanceTimer.Tick += CalculateFPS;
+        _performanceTimer.Start();
+
+        // UI 스레드 블로킹 감지
+        EnableUIThreadBlockingDetection();
+    }
+
+    private void OnRendering(object sender, EventArgs e)
+    {
+        _frameCount++;
+    }
+
+    private void CalculateFPS(object sender, EventArgs e)
+    {
+        var now = DateTime.Now;
+        var elapsed = (now - _lastFrameTime).TotalSeconds;
+        var fps = _frameCount / elapsed;
+
+        Debug.WriteLine($"[FPS] {fps:F2} fps");
+
+        _frameCount = 0;
+        _lastFrameTime = now;
+    }
+
+    private void EnableUIThreadBlockingDetection()
+    {
+        var timer = new DispatcherTimer(DispatcherPriority.Send);
+        timer.Interval = TimeSpan.FromMilliseconds(100);
+        var lastTick = DateTime.Now;
+
+        timer.Tick += (s, e) =>
+        {
+            var now = DateTime.Now;
+            var elapsed = (now - lastTick).TotalMilliseconds;
+
+            if (elapsed > 150) // 150ms 이상 블로킹
+            {
+                Debug.WriteLine($"[경고] UI 스레드 블로킹 감지: {elapsed:F0}ms");
+            }
+
+            lastTick = now;
+        };
+
+        timer.Start();
+    }
+}
---
+++ testpro/PerformanceLogger.cs
@@ -0,0 +1,29 @@
+﻿// PerformanceLogger.cs - ETW를 사용한 성능 로깅
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Windows.Threading;
+
+[EventSource(Name = "SmartShelf-Performance")]
+public sealed class PerformanceEventSource : EventSource
+{
+    public static PerformanceEventSource Log = new PerformanceEventSource();
+
+    [Event(1, Level = EventLevel.Informational)]
+    public void UIInputStart(string toolName)
+    {
+        WriteEvent(1, toolName);
+    }
+
+    [Event(2, Level = EventLevel.Informational)]
+    public void UIInputEnd(string toolName, double elapsedMs)
+    {
+        WriteEvent(2, toolName, elapsedMs);
+    }
+
+    [Event(3, Level = EventLevel.Warning)]
+    public void SlowUIResponse(string toolName, double elapsedMs)
+    {
+        WriteEvent(3, toolName, elapsedMs);
+    }
+}
